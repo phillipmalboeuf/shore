fd2cb0b6e418d257e6ffe54901756967f3fce85d
Phillip Malboeuf <phil@boeuf.coffee>
2016-02-20 21:26:54 -0500

    Development

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 (function() {
   $(function() {
-    return console.log("INIT");
+    console.log("I like this digital age.");
+    return $(document).click("js-scroll_to", function(e) {
+      return console.log(e.currentTarget);
+    });
   });
 
 }).call(this);
--- scripts/coffee/app.coffee
+++ scripts/coffee/app.coffee
@@ -1,4 +1,8 @@
 
 
 $ ->
-	console.log "INIT"
+	console.log "I like this digital age."
+
+
+	$(document).click "js-scroll_to", (e)->
+		console.log e.currentTarget 
