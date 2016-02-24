4e7fc6799cca02204e04488789e5d3c5a27508d6
Phillip Malboeuf <phil@boeuf.coffee>
2016-02-24 17:00:07 +0900

    Development

--- scripts/app.js
+++ scripts/app.js
@@ -4,12 +4,17 @@
     return $("[data-scroll-to]").click(function(e) {
       var scroll_to;
       scroll_to = $("#" + e.currentTarget.getAttribute("data-scroll-to"));
-      if (scroll_to.length > 0 && !scroll_to.hasClass("biography")) {
-        e.preventDefault();
-        return scroll_to.velocity("scroll", {
-          duration: 2000,
-          easing: "easeOutQuart"
-        });
+      if (scroll_to.length > 0) {
+        console.log(scroll_to.css("visibility"));
+        if (scroll_to.hasClass("biography")) {
+
+        } else {
+          e.preventDefault();
+          return scroll_to.velocity("scroll", {
+            duration: 2000,
+            easing: "easeOutQuart"
+          });
+        }
       }
     });
   });
--- scripts/coffee/app.coffee
+++ scripts/coffee/app.coffee
@@ -7,6 +7,10 @@ $ ->
 	$("[data-scroll-to]").click (e)->
 		scroll_to = $("#"+e.currentTarget.getAttribute("data-scroll-to"))
 
-		if scroll_to.length > 0 and !scroll_to.hasClass("biography")
-			e.preventDefault()
-			scroll_to.velocity("scroll", { duration: 2000, easing: "easeOutQuart" })
+		if scroll_to.length > 0
+			console.log scroll_to.css("visibility")
+			if scroll_to.hasClass("biography")
+
+			else
+				e.preventDefault()
+				scroll_to.velocity("scroll", { duration: 2000, easing: "easeOutQuart" })
