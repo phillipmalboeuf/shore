841536fc7f9afbd31607961c2511ea470714e6f5
Phillip Malboeuf <phil@boeuf.coffee>
2016-02-24 16:58:57 +0900

    Development

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@
     return $("[data-scroll-to]").click(function(e) {
       var scroll_to;
       scroll_to = $("#" + e.currentTarget.getAttribute("data-scroll-to"));
-      if (scroll_to.length > 0) {
+      if (scroll_to.length > 0 && !scroll_to.hasClass("biography")) {
         e.preventDefault();
         return scroll_to.velocity("scroll", {
           duration: 2000,
--- scripts/coffee/app.coffee
+++ scripts/coffee/app.coffee
@@ -7,6 +7,6 @@ $ ->
 	$("[data-scroll-to]").click (e)->
 		scroll_to = $("#"+e.currentTarget.getAttribute("data-scroll-to"))
 
-		if scroll_to.length > 0
+		if scroll_to.length > 0 and !scroll_to.hasClass("biography")
 			e.preventDefault()
 			scroll_to.velocity("scroll", { duration: 2000, easing: "easeOutQuart" })
