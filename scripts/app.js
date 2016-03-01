e690a760388604c24cd2c22d88088883270e1c30
Phillip Malboeuf <phil@boeuf.coffee>
2016-03-01 19:20:23 +0900

    Development

--- scripts/app.js
+++ scripts/app.js
@@ -56,7 +56,32 @@
 }).call(this);
 
 (function() {
+  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
 
+  Daniela.Views.Header = (function(superClass) {
+    extend(Header, superClass);
+
+    function Header() {
+      return Header.__super__.constructor.apply(this, arguments);
+    }
+
+    Header.prototype.events = {};
+
+    Header.prototype.initialize = function(options) {
+      if (options == null) {
+        options = {};
+      }
+      return this.render();
+    };
+
+    Header.prototype.render = function() {
+      return this;
+    };
+
+    return Header;
+
+  })(Backbone.View);
 
 }).call(this);
 
---
+++ scripts/coffee/views/header.coffee
@@ -0,0 +1,20 @@
+class Daniela.Views.Header extends Backbone.View
+
+
+	events: {
+
+	}
+
+
+
+	initialize: (options={})->
+
+
+		this.render()
+
+
+
+	render: ->
+
+
+		this
