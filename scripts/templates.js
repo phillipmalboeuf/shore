60902ff4ddc8a6d3650284621f4209d9ae8547f4
Phillip Malboeuf <phil@boeuf.coffee>
2016-03-02 19:54:14 +0900

    Development

--- scripts/templates.js
+++ scripts/templates.js
@@ -3,7 +3,7 @@ this["templates"] = this["templates"] || {};
 this["templates"]["social_links"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class=\"col col--3of12\">\n<a href=\""
+  return "<div class=\"col\">\n<a href=\""
     + alias4(((helper = (helper = helpers.profile_url || (depth0 != null ? depth0.profile_url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"profile_url","hash":{},"data":data}) : helper)))
     + "\" target=\"_blank\"><svg class=\"icon-"
     + alias4(((helper = (helper = helpers.service_name || (depth0 != null ? depth0.service_name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"service_name","hash":{},"data":data}) : helper)))
