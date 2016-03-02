a520f0b721e4d77b949bc221c1615c0369c07565
Phillip Malboeuf <phil@boeuf.coffee>
2016-03-02 19:43:26 +0900

    Development

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,10 +1,13 @@
 this["templates"] = this["templates"] || {};
 
 this["templates"]["social_links"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
-    var helper;
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return container.escapeExpression(((helper = (helper = helpers.service_name || (depth0 != null ? depth0.service_name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"service_name","hash":{},"data":data}) : helper)))
-    + "\n";
+  return "<a href=\"{profile_url}\" target=\"_blank\"><svg class=\"icon-"
+    + alias4(((helper = (helper = helpers.service_name || (depth0 != null ? depth0.service_name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"service_name","hash":{},"data":data}) : helper)))
+    + "\"><use xlink:href=\"/assets/icons.svg#icon-"
+    + alias4(((helper = (helper = helpers.service_name || (depth0 != null ? depth0.service_name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"service_name","hash":{},"data":data}) : helper)))
+    + "\"></use></svg></a>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
