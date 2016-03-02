c7ba01de4243544dd6fba386085b5281bbf47d96
Phillip Malboeuf <phil@boeuf.coffee>
2016-03-02 19:48:57 +0900

    Development

--- scripts/templates.js
+++ scripts/templates.js
@@ -13,7 +13,7 @@ this["templates"]["social_links"] = Handlebars.template({"1":function(container,
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div class=\"grid\">\n"
+  return "<div class=\"grid grid--guttered\">\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.links : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "</div>\n";
 },"useData":true});
