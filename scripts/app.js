98d72c7035ce9a6fd034c84ccc6401a0f6237934
Phillip Malboeuf <phil@boeuf.coffee>
2016-03-09 11:50:31 +0900

    Development

--- scripts/app.js
+++ scripts/app.js
@@ -141,6 +141,18 @@
       var header, previous_offset;
       header = this.$el;
       previous_offset = 0;
+      $(window).scroll(function(e) {
+        if (window.pageYOffset > previous_offset) {
+          if (!header.hasClass("header--hide")) {
+            header.addClass("header--hide");
+          }
+        } else {
+          if (header.hasClass("header--hide")) {
+            header.removeClass("header--hide");
+          }
+        }
+        return previous_offset = window.pageYOffset;
+      });
       return Header.__super__.render.call(this);
     };
 
